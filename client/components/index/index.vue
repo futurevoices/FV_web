6a8b7ac5514ccf42fb526ca04c051929ee8b0e30
Vinzenz Aubry <vinzenzaubry@gmail.com>
2021-01-16 19:31:26 +0100

    started adding desktop

--- client/components/index/index.vue
+++ client/components/index/index.vue
@@ -1,47 +1,63 @@
 <template>
   <section class="">
-    <div class="container mb-4">
-      <div class="row mt-2 mb-1">
-        <div class="col-md-12">
-          <p>
-            We live in a present with many, very different possible futures.
-            They range from doom to bliss, dystopia to utopia.
-            <br /><br />
-            What do YOU think about the future? What do YOU expect, fear, hope,
-            propose, plan for the future?
-            <br /><br />
-            You can make your voice heard: We are collecting a multitude of
-            voices that compose a 1-year-long sound stream.
-            <br /><br />
-            Listen to the stream:
-          </p>
-          <p>
-            <br /><br />
-            Add your own recording
-            <span
-              ><nuxt-link to="/upload" class="nav-link">here</nuxt-link></span
-            >.
-          </p>
-
-          <p>Invite people to participate by mail <a href="mailto:?subject=Future%20Voices%20Radio&body=Hello%2CI'd%20like%20to%20invite%20you%20to%20participate%20with%20a%20recording%20about%20the%20future%20at%20https%3A%2F%2Ffuturevoices.radio%2F.Enjoy!">here</a></p>.
-        </div>
+    <div class="contentWrapper">
+      <p>
+        Welcome to Future Voices / Zukunftsmusik!
+        <br /><br />
+        A one-year live-generated radio stream composed from hopes, fears and
+        dreams of contributors from around the world.
+      </p>
+      <div class="cta-links">
+        <p>Listen</p>
+        <nuxt-link to="/upload">Contribute</nuxt-link>
+        <a
+          href="mailto:?subject=Future%20Voices%20Radio&body=Hello%2CI'd%20like%20to%20invite%20you%20to%20participate%20with%20a%20recording%20about%20the%20future%20at%20https%3A%2F%2Ffuturevoices.radio%2F.Enjoy!"
+          >Share</a
+        >
       </div>
+      <p>
+        We live in a present with many, very different possible futures. They
+        range from doom to bliss, dystopia to utopia.
+      </p>
+      <p>
+        What do YOU think about the future? What do YOU expect, fear, hope,
+        propose, plan for the future? You can make your voice heard: We are
+        collecting a multitude of voices that together compose this
+        one-year-long sound stream.
+      </p>
     </div>
   </section>
 </template>
 
 <style lang="scss" scoped>
-p {
-
-  text-align: left;
-  max-width: 500px;
-  position: relative;
+.contentWrapper {
+  padding-top: 20vh;
+  width: 100%;
+  max-width: 900px;
   margin: 0 auto;
-  margin-bottom: 24px;
-  span a {
-    margin: 0 !important;
-    padding: 0 !important;
-    display: inline-block !important;
+  text-align: left;
+
+  p,
+  a {
+    font-size: 24px;
+    line-height: 125%;
+  }
+  .cta-links {
+    display: flex;
+    justify-content: space-around;
+    padding-top: 24px;
+    padding-bottom: 24px;
+    p,
+    a {
+      display: inline-block;
+      text-decoration: underline;
+      cursor: pointer;
+    }
+
+    p:hover,
+    a:hover {
+      font-style: italic;
+    }
   }
 }
 </style>
@@ -54,7 +70,7 @@ export default {
   computed: {},
   methods: {},
   mounted() {
-    console.log("stream has been mounted")
+    console.log('stream has been mounted');
   },
   created() {}
 };
