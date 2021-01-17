2707d21526b76e700d02b58c800044d934b5b4a9
Vinzenz Aubry <vinzenzaubry@gmail.com>
2021-01-17 13:53:50 +0100

    added gradient

--- client/components/index/index.vue
+++ client/components/index/index.vue
@@ -1,70 +1,105 @@
 <template>
   <section class="">
+    <div id="background" class="background-image">
+      <div class="inside-shadow"></div>
+      <img src="background-image.png" alt="" />
+    </div>
     <div class="contentWrapper">
-      <div class="background-image">
-        <div class="inside-shadow"></div>
-        <img src="background-image.png" alt="" />
+      <div id="introText" class="content">
+        <div class="moebius"><Moebius /></div>
+        <p>
+          Welcome to Future Voices / Zukunftsmusik!
+          <br /><br />
+          A one-year live-generated radio stream composed from hopes, fears and
+          dreams of contributors from around the world.
+        </p>
+        <div class="cta-links">
+          <p>Listen</p>
+          <nuxt-link to="/upload">Contribute</nuxt-link>
+          <a
+            href="mailto:?subject=Future%20Voices%20Radio&body=Hello%2CI'd%20like%20to%20invite%20you%20to%20participate%20with%20a%20recording%20about%20the%20future%20at%20https%3A%2F%2Ffuturevoices.radio%2F.%20Enjoy!"
+            >Share</a
+          >
+        </div>
+        <p>
+          We live in a present with many, very different possible futures. They
+          range from doom to bliss, dystopia to utopia.
+        </p>
+        <p>
+          What do YOU think about the future? What do YOU expect, fear, hope,
+          propose, plan for the future? You can make your voice heard: We are
+          collecting a multitude of voices that together compose this
+          one-year-long sound stream.
+        </p>
       </div>
-
-      <Moebius />
-      <p>
-        Welcome to Future Voices / Zukunftsmusik!
-        <br /><br />
-        A one-year live-generated radio stream composed from hopes, fears and
-        dreams of contributors from around the world.
-      </p>
-      <div class="cta-links">
-        <p>Listen</p>
-        <nuxt-link to="/upload">Contribute</nuxt-link>
-        <a
-          href="mailto:?subject=Future%20Voices%20Radio&body=Hello%2CI'd%20like%20to%20invite%20you%20to%20participate%20with%20a%20recording%20about%20the%20future%20at%20https%3A%2F%2Ffuturevoices.radio%2F.%20Enjoy!"
-          >Share</a
-        >
-      </div>
-      <p>
-        We live in a present with many, very different possible futures. They
-        range from doom to bliss, dystopia to utopia.
-      </p>
-      <p>
-        What do YOU think about the future? What do YOU expect, fear, hope,
-        propose, plan for the future? You can make your voice heard: We are
-        collecting a multitude of voices that together compose this
-        one-year-long sound stream.
-      </p>
     </div>
   </section>
 </template>
 
 <style lang="scss" scoped>
+.background-image {
+  top: 40px;
+  left: 0;
+  position: absolute;
+  display: block;
+  width: 100%;
+  height: auto;
+  z-index: 0;
+
+  .inside-shadow {
+    position: absolute;
+    top: 0;
+    width: inherit;
+    height: inherit;
+    box-shadow: inset 0px 0px 0px 5px $lightweight,
+      // first one is to hide the gap
+        inset 0px 0px 30px 40px $lightweight;
+    mix-blend-mode: normal;
+    z-index: 2;
+  }
+
+  img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: inherit;
+    height: inherit;
+    object-fit: cover;
+    opacity: 1;
+    z-index: 1;
+  }
+}
 .contentWrapper {
+  position: relative;
   width: 100%;
   max-width: 900px;
   margin: 0 auto;
   text-align: left;
+  background-color: yellow;
+  margin-bottom: 40px;
 
-  .background-image {
-    position: relative;
-    display: block;
+  .content {
+    position: absolute;
+    top: 0;
+    left: 0;
     width: 100%;
-    height: 500px;
+    height: auto;
+    z-index: 4;
+    margin-bottom: 40px;
 
-    .inside-shadow {
-      position: absolute;
-      top: 0;
-      width: inherit;
-      height: inherit;
-      box-shadow: inset 0px 0px 0px 5px $lightweight,
-        // first one is to hide the gap
-          inset 0px 0px 30px 40px $lightweight;
-      mix-blend-mode: normal;
+    p,
+    a {
+      color: $lightweight;
     }
+  }
 
-    img {
-      width: inherit;
-      height: inherit;
-      object-fit: cover;
-      opacity: 1;
-    }
+  .moebius {
+    // background-color: yellow;
+    width: 100%;
+    height: auto;
+    top: 0;
+    left: 0;
+    z-index: 3;
   }
 
   p,
@@ -103,8 +138,22 @@ export default {
     return {};
   },
   computed: {},
-  methods: {},
-  mounted() {},
-  created() {}
+  methods: {
+    resize() {
+      // resize the background image
+      let height = document.getElementById('introText').offsetHeight;
+      height += 100;
+      document.getElementById('background').style.height = height + 'px';
+      console.log(height);
+    }
+  },
+  mounted() {
+    this.resize();
+    window.addEventListener('resize', this.resize);
+  },
+  created() {},
+  destroyed() {
+    window.removeEventListener('resize', this.resize);
+  }
 };
 </script>
